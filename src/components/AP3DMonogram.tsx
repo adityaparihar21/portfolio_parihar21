d1f3a2b395a79f9ecf75121caf7def3525a1e2d3
Aditya Parihar <parihar21aditya@gmail.com>
2026-06-19 03:19:53 +0530

    Shift coin right to optically center it on the page

--- src/components/AP3DMonogram.tsx
+++ src/components/AP3DMonogram.tsx
@@ -196,7 +196,7 @@ function APCoin() {
   };
 
   return (
-    <group scale={0} ref={coinRef}> {/* Starts at 0, spring-animated to TARGET_SCALE */}
+    <group scale={0} ref={coinRef} position={[0.15, 0, 0]}> {/* Nudged right to optically center the AP text */}
       {/* The Solid Coin Base */}
       <mesh rotation={[Math.PI / 2, 0, 0]} castShadow receiveShadow>
         <cylinderGeometry args={[2.0, 2.0, 0.35, 128]} />
