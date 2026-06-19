1a181a52758f517f535e95d5135ca5635e23a725
Aditya Parihar <parihar21aditya@gmail.com>
2026-06-19 16:28:59 +0530

    Visually shift 3D focal point to the right to align with asymmetrical video background

--- src/components/AP3DMonogram.tsx
+++ src/components/AP3DMonogram.tsx
@@ -345,11 +345,14 @@ export default function AP3DMonogram({ className = '' }: { className?: string })
           {/* Studio HDRI for strong, clean gold reflections */}
           <Environment preset="studio" />
 
-          <APCoin />
+          {/* Shift entire 3D focal point to the right to visually align with the background video */}
+          <group position={[0.25, 0, 0]}>
+            <APCoin />
+          </group>
 
           {/* Contact shadow grounds the coin instead of floating */}
           <ContactShadows
-            position={[0, -1.3, 0]}
+            position={[0.25, -1.3, 0]}
             opacity={0.35}
             blur={2}
             scale={10}
@@ -361,7 +364,7 @@ export default function AP3DMonogram({ className = '' }: { className?: string })
             enableZoom={false}
             enablePan={false}
             enableRotate={true}
-            target={[0, 0, 0]}
+            target={[0.25, 0, 0]}
             minPolarAngle={Math.PI / 2 - 0.4} // Allow slight upward tilt (drifting)
             maxPolarAngle={Math.PI / 2 + 0.4} // Allow slight downward tilt
             makeDefault
